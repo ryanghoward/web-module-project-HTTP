68da82b96a0ec6cb0524c5a8c5c2821caff8671e
Ryan Howard <ryanghoward@protonmail.com>
2022-01-26 14:04:40 -0800

    Edit Movie fixed and Delete Movie functional

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+    setMovies(movies.filter(item => (item.id !== Number(id))));
   }
 
   const addToFavorites = (movie) => {
@@ -42,11 +43,11 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
-              <EditMovieForm />
+              <EditMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
             <Route path="/movies">
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const Movie = (props) => {
     const { addToFavorites } = props;
+    const { deleteMovie } = props;
 
     const [movie, setMovie] = useState('');
 
@@ -20,6 +21,18 @@ const Movie = (props) => {
                 console.log(err.response);
             })
     }, [id]);
+
+    const handleDelete = () => {
+        axios.delete(`http://localhost:9000/api/movies/${id}`)
+            .then(resp => {
+                console.log(props);
+                deleteMovie(id);
+                push('/movies');
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
 
     return(<div className="modal-page col">
         <div className="modal-dialog">
@@ -52,7 +65,7 @@ const Movie = (props) => {
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
                             <Link to={`/movies/edit/${movie.id}`} className="m-2 btn btn-success">Edit</Link>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span onClick={handleDelete} className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
                 </div>
